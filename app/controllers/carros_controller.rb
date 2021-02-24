b0dda3bd3807164dd015f7607d932470d454c0a3
Leonardo Fragozo <lqfragozo@gmail.com>
2021-02-24 11:58:26 -0300

    Crud adicionando mas nao redirecionando

--- app/controllers/carros_controller.rb
+++ app/controllers/carros_controller.rb
@@ -1,10 +1,30 @@
 class CarrosController < ApplicationController
+  def index
+    @carros = Carro.all
+  end
+
   def show
+    @carro = Carro.find(params[:id])
   end
 
   def new
+    @carro = Carro.new
   end
 
-  def edit
+  def create
+    carro =  Carro.create(carro_params)
+
+    redirect_to carros.path
+  end
+
+
+  def edit 
+
+  end
+
+  private
+
+  def carro_params
+    params.require(:carro).permit(:marca, :modelo, :ano, :descricao, :valor)
   end
 end
