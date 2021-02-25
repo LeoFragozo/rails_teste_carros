63f1c4836eb6d14cfc28eee427e6649418bf2eff
Leonardo Fragozo <lqfragozo@gmail.com>
2021-02-25 11:35:45 -0300

    CRuD e Views com partials ok

--- app/controllers/carros_controller.rb
+++ app/controllers/carros_controller.rb
@@ -17,8 +17,29 @@ class CarrosController < ApplicationController
     if @carro.save
       redirect_to @carro
     else
-      render :new
+      render :new 
     end
+  end
+
+  def edit
+    @carro = Carro.find(params[:id])
+  end
+
+  def update
+    @carro = Carro.find(params[:id])
+
+    if @carro.update(carro_params)
+      redirect_to @carro
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @carro = Carro.find(params[:id])
+    @carro.destroy
+
+    redirect_to root_path
   end
 
   private
